bef0eda009182867faf3c2dfae5bb0c426c21d04
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-22 18:11:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (action = "go to the office") {
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective (val = "*") {
+    const inF = function (value = "special") {
+        return `You are ${val}${value}${val}!`
+    }
+    return inF;
+}
